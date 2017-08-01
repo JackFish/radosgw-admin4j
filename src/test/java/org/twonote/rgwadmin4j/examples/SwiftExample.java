261448c6606aa25429d21061dba8c290fd4523f6
jack_zhou <zj_ren8@163.com>
2017-08-01 23:42:11 +0800

    create 2 account

--- src/test/java/org/twonote/rgwadmin4j/examples/SwiftExample.java
+++ src/test/java/org/twonote/rgwadmin4j/examples/SwiftExample.java
@@ -21,97 +21,111 @@ import java.util.*;
  * Created by petertc on 3/31/17.
  */
 public class SwiftExample {
-  private static String username;
-  private static String password;
-  private static String authUrl;
-  private static final String userId = "rgwAdmin4jTest-" + UUID.randomUUID().toString();
-  private static RgwAdmin RGW_ADMIN_CLIENT;
+    private static String username;
+    private static String password;
+    private static String authUrl;
+    private static final String userId = "rgwAdmin4jTest-" + UUID.randomUUID().toString();
+    private static RgwAdmin RGW_ADMIN_CLIENT;
+    private static String adminUserName;
+    private static String adminPassword;
 
-  private static void example() {
-    // Create a Connection
-    AccountConfig config = new AccountConfig();
-    config.setUsername(username);
-    config.setPassword(password);
-    config.setAuthUrl(authUrl);
-    config.setAuthenticationMethod(AuthenticationMethod.BASIC);
-    Account account = new AccountFactory(config).createAccount();
+    private static void example() {
+        // Create a Connection
+        AccountConfig config = new AccountConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setAuthUrl(authUrl);
+        config.setAuthenticationMethod(AuthenticationMethod.BASIC);
+        Account suzhou = new AccountFactory(config).createAccount();
+        AccountConfig config1 = new AccountConfig();
+        config1.setUsername(adminUserName);
+        config1.setPassword(adminPassword);
+        config1.setAuthUrl(authUrl);
+        config1.setAuthenticationMethod(AuthenticationMethod.BASIC);
+        Account najing = new AccountFactory(config1).createAccount();
 
-    // Create a Container
-    Container container = account.getContainer("my-new-container");
-    container.create();
+        // Create a Container
+        Container container = suzhou.getContainer("my-new-container11");
+        container.create();
 
-    // Create an Object
-    StoredObject object = container.getObject("foo.txt");
-    object.uploadObject(new File("src/test/resources/foo.txt"));
+        // Create an Object
+        StoredObject object = container.getObject("foo.txt");
+        object.uploadObject(new File("src/test/resources/foo.txt"));
 
-    // Add/Update Object Metadata
-    Map<String, Object> metadata = new TreeMap<String, Object>();
-    metadata.put("key", "value");
-    object.setMetadata(metadata);
+        // Add/Update Object Metadata
+        Map<String, Object> metadata = new TreeMap<String, Object>();
+        metadata.put("key", "value");
+        object.setMetadata(metadata);
 
-    // List Owned Containers
-    Collection<Container> containers = account.list();
-    for (Container currentContainer : containers) {
-      System.out.println(currentContainer.getName());
+        // List Owned Containers
+        Collection<Container> containers = najing.list();
+        for (Container currentContainer : containers) {
+            System.out.println(currentContainer.getName());
+        }
+
+        // List a Container's Content
+        Collection<StoredObject> objects = container.list();
+        for (StoredObject currentObject : objects) {
+            System.out.println(currentObject.getName());
+        }
+
+        // Retrieve an Object's Metadata
+        Map<String, Object> returnedMetadata = object.getMetadata();
+        for (String name : returnedMetadata.keySet()) {
+            System.out.println("META / " + name + ": " + returnedMetadata.get(name));
+        }
+
+        // Retrieve an Object
+        object.downloadObject(new File("/tmp/outfile.txt"));
+
+        // Delete an Object
+        object.delete();
+
+        // Delete a Container
+        container.delete();
     }
 
-    // List a Container's Content
-    Collection<StoredObject> objects = container.list();
-    for (StoredObject currentObject : objects) {
-      System.out.println(currentObject.getName());
+    private static void init() throws IOException {
+        String env = System.getProperty("env", "");
+        if (!"".equals(env)) {
+            env = "." + env;
+        }
+        Properties properties = new Properties();
+        properties.load(SwiftExample.class.getResourceAsStream("/rgwadmin.properties" + env));
+
+        String adminUserId = properties.getProperty("radosgw.adminId");
+        String accessKey = properties.getProperty("radosgw.adminAccessKey");
+        String secretKey = properties.getProperty("radosgw.adminSecretKey");
+        String s3Endpoint = properties.getProperty("radosgw.endpoint");
+        String adminEndpoint = properties.getProperty("radosgw.adminEndpoint");
+        authUrl = s3Endpoint + "/auth/1.0";
+
+        RGW_ADMIN_CLIENT = new RgwAdminImpl(accessKey, secretKey, adminEndpoint);
+        User response = RGW_ADMIN_CLIENT.createUser(userId);
+
+
+        String subUserId = UUID.randomUUID().toString();
+        RGW_ADMIN_CLIENT.createSubUser(response.getUserId(), subUserId, SubUser.Permission.FULL, CredentialType.SWIFT);
+
+        User response2 = RGW_ADMIN_CLIENT.getUserInfo(response.getUserId()).get();
+        username = response2.getSwiftCredentials().get(0).getUsername();
+        password = response2.getSwiftCredentials().get(0).getPassword();
+
+
+        RGW_ADMIN_CLIENT.createSubUser(response.getUserId(), UUID.randomUUID().toString(), SubUser.Permission.READ, CredentialType.SWIFT);
+        User response3 = RGW_ADMIN_CLIENT.getUserInfo(response.getUserId()).get();
+        adminUserName = response3.getSwiftCredentials().get(0).getUsername();
+        adminPassword = response3.getSwiftCredentials().get(0).getPassword();
     }
 
-    // Retrieve an Object's Metadata
-    Map<String, Object> returnedMetadata = object.getMetadata();
-    for (String name : returnedMetadata.keySet()) {
-      System.out.println("META / " + name + ": " + returnedMetadata.get(name));
+    public static void main(String[] args) throws IOException {
+        init();
+        try {
+            example();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            RGW_ADMIN_CLIENT.removeUser(userId);
+        }
     }
-
-    // Retrieve an Object
-    object.downloadObject(new File("/tmp/outfile.txt"));
-
-    // Delete an Object
-    object.delete();
-
-    // Delete a Container
-    container.delete();
-  }
-
-  private static void init() throws IOException {
-    String env = System.getProperty("env", "");
-    if (!"".equals(env)) {
-      env = "." + env;
-    }
-    Properties properties = new Properties();
-    properties.load(SwiftExample.class.getResourceAsStream("/rgwadmin.properties" + env));
-
-    String adminUserId = properties.getProperty("radosgw.adminId");
-    String accessKey = properties.getProperty("radosgw.adminAccessKey");
-    String secretKey = properties.getProperty("radosgw.adminSecretKey");
-    String s3Endpoint = properties.getProperty("radosgw.endpoint");
-    String adminEndpoint = properties.getProperty("radosgw.adminEndpoint");
-    authUrl = s3Endpoint + "/auth/1.0";
-
-    RGW_ADMIN_CLIENT = new RgwAdminImpl(accessKey, secretKey, adminEndpoint);
-    User response = RGW_ADMIN_CLIENT.createUser(userId);
-
-    String subUserId = UUID.randomUUID().toString();
-    RGW_ADMIN_CLIENT.createSubUser(response.getUserId(), subUserId, SubUser.Permission.FULL, CredentialType.SWIFT);
-
-    User response2 = RGW_ADMIN_CLIENT.getUserInfo(response.getUserId()).get();
-    username = response2.getSwiftCredentials().get(0).getUsername();
-    password = response2.getSwiftCredentials().get(0).getPassword();
-  }
-
-  public static void main(String[] args) throws IOException {
-    init();
-    try {
-      example();
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      RGW_ADMIN_CLIENT.removeUser(userId);
-    }
-
-  }
 }
